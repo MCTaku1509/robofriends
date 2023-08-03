6ae581e17d66e20d2a3c73a9539f0c1823894b9e
MCTaku1509 <119457400+MCTaku1509@users.noreply.github.com>
2023-08-03 10:27:44 +0100

    creating the card component

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+//creating card component
+import React from "react";
+
+const Card = () => {
+    return (
+        <div>
+            <img src='' />
+            <div>
+                <h2>Name Surname</h2>
+                <p>name.surname@email.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
